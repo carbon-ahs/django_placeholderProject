c8521f205785702406183f158476052860e1a349
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-15 21:56:53 +0600

    custom user has time mixin

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     pass
